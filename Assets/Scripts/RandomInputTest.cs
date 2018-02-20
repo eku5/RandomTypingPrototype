b0ae372dd18d65cba03358ee02c4629c3960cdff
E. Ung <evee.ung@gmail.com>
2018-02-20 16:45:13 -0500

    Updated RandomInputTest script.

--- Assets/Scripts/RandomInputTest.cs
+++ Assets/Scripts/RandomInputTest.cs
@@ -6,11 +6,18 @@ public class RandomInputTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+	//Check if the KeyCode is a letter
+		for (int i = 0;i < 26;i++){
+			
+			if (Input.GetKeyUp(KeyCode.A + i))
+			print("gibberish");
 		
+
 	}
 }
